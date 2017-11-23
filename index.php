68a1638faf07a57c881c4f24b1419a7ce66b399a
Riemer Palstra <riemer@palstra.com>
2017-11-23 13:49:40 +0100

    Remove some variables.

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <html>
 
 <head>
-
 <title>Hello World!</title>
-
 </head>
 
 <body>
 
-<?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"World")."!"; ?>
+<?php echo "Hello World!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?>
 
 <?php $links = [];
