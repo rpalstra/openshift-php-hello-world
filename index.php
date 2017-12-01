ce52df5d6cb5a30171c66958a6d31904811e4123
Riemer Palstra <riemer@palstra.com>
2017-12-01 15:51:03 +0100

    Test branch, first commit.

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
   foreach($_ENV as $key => $value) {
     if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
       $links[] = [
-        "name" => $matches[1],
-        "port" => $matches[2],
+        "name"  => $matches[1],
+        "port"  => $matches[2],
         "proto" => $matches[3],
         "value" => $value
       ];
