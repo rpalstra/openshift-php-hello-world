2669166a825dd444009fa1413af89cd86ffbd07f
tazvoit <tazvoit@gmail.com>
2019-07-26 17:22:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "Examen OCP!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
